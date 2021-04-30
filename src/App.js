4e545feb5c8f9313bf42f879424a2617a006d0a8
saichandra2896 <saichandra2896@gmail.com>
2021-04-30 17:45:16 +0530

    add api call using axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import Header from "./components/header/Header";
 import FilterSection from "./components/filter/FilterSection";
+import Posts from "./components/posts/Posts";
 
 import "./App.css";
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    console.log("122");
+    const fetchData = async () => {
+      try {
+        const resData = await axios.get(
+          "https://raw.githubusercontent.com/Lokenath/MyRepo/master/Test/package.json"
+        );
+        const postData = resData.data.pics;
+        // console.log(postData);
+        setPosts(postData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <FilterSection />
+      <Posts posts={posts} />
     </div>
   );
 };
