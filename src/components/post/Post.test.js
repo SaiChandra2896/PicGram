280fbcb2e59a9ef0359fa11615424a01f2c4f863
saichandra2896 <saichandra2896@gmail.com>
2021-05-03 11:15:19 +0530

    add readme

--- src/components/post/Post.test.js
+++ src/components/post/Post.test.js
@@ -1,6 +1,1 @@
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-import Post from "./Post";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
